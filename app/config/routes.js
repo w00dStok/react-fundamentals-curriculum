ba5507f64055fc473b89a05c61decf97c93f5937
w00dStok <woodmatthewj@yahoo.com>
2016-09-12 19:41:22 -0500

    First Step: Make a Hello World component

---
+++ app/config/routes.js
@@ -0,0 +1,10 @@
+// var React = require('react');
+// var ReactRouter = require('react-router');
+// var Router = ReactRouter.Router;
+// var Router = ReactRouter.Route;
+
+// var routes = (
+//     <Router />
+// );
+
+// module.exports = routes;
---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var HelloWorld = require('./components/Main');
+//var routes = require('./config/routes');
+
+ReactDOM.render(
+    <HelloWorld />, document.getElementById('app')
+);
